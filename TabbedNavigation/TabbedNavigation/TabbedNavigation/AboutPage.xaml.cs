134dfa6972779371339b2e92f6e59d53f1c7a561
Ethan Gutknecht <93880357+ethangutknecht@users.noreply.github.com>
2022-07-26 00:37:18 -0400

    Add files via upload

---
+++ TabbedNavigation/TabbedNavigation/TabbedNavigation/AboutPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TabbedNavigation {
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AboutPage : ContentPage {
+        public AboutPage() {
+            InitializeComponent();
+            Padding = new Thickness(0, 50, 0, 0);
+        }
+    }
+}
---
+++ TabbedNavigation/TabbedNavigation/TabbedNavigation/RegistrationPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TabbedNavigation {
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class RegistrationPage : ContentPage {
+        public RegistrationPage() {
+            InitializeComponent();
+        }
+        public string Name {
+            get { return name.Text; }
+        }
+    }
+}
