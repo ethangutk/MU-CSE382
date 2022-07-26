de8406ebf868e337080049795331a97dc847da90
Ethan Gutknecht <93880357+ethangutknecht@users.noreply.github.com>
2022-07-26 00:44:39 -0400

    Add files via upload

---
+++ SoundApp/SoundApp/SoundApp/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using Xamarin.Forms;
+using Plugin.SimpleAudioPlayer;
+using System.IO;
+using System.Reflection;
+using SoundApp;
+using System;
+using System.ComponentModel;
+
+namespace SoundApp
+{
+	[DesignTimeVisible(false)]
+	public partial class MainPage : ContentPage
+	{
+		ISimpleAudioPlayer player = CrossSimpleAudioPlayer.CreateSimpleAudioPlayer();
+		public MainPage()
+		{
+			InitializeComponent();
+			Load("sound.mp3");
+		}
+		private void Load(string file)
+		{
+			var assembly = typeof(App).GetTypeInfo().Assembly;
+			String ns = "Audio";
+			Stream audioStream = assembly.GetManifestResourceStream(ns + "." + file);
+			player.Load(audioStream);
+		}
+		private void OnClicked(object sender, EventArgs e)
+		{
+			player.Play();
+		}
+
+        private void stopButton_Clicked(object sender, EventArgs e)
+        {
+			player.Stop();
+		}
+
+        private void loopSwitch_Toggled(object sender, ToggledEventArgs e)
+        {
+			if (loopSwitch.IsToggled)
+			{
+				player.Loop = true;
+			}
+			else player.Loop = false;
+        }
+    }
+}
