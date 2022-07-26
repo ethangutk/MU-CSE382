85bb1477619133da8d39602c65e79fe5a5233529
Ethan Gutknecht <93880357+ethangutknecht@users.noreply.github.com>
2022-07-26 00:22:38 -0400

    Add files via upload

---
+++ Carousel/Carousel/Carousel/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Carousel {
+    public partial class App : Application {
+        public string[] urls = { "www.miamioh.edu", "www.osu.edu", "www.uc.edu" };
+        public App() {
+            InitializeComponent();
+
+            CarouselPage carouselPage = new CarouselPage();
+            foreach (string url in urls) {
+                School schoolPage = new School("https://" + url);
+                schoolPage.Padding = new Thickness(0, 40, 0, 0);
+                carouselPage.Children.Add(schoolPage);
+            }
+            MainPage = carouselPage;
+        }
+
+        protected override void OnStart() {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep() {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume() {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ Carousel/Carousel/Carousel/School.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Carousel {
+    class School : ContentPage {
+        public School(String url) {
+            WebView wv = new WebView { Source = url };
+            this.Content = wv;
+        }
+    }
+}
