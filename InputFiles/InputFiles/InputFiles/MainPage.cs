39c38198bbcc688caab89d03a88c8f3218d53e78
Ethan Gutknecht <93880357+ethangutknecht@users.noreply.github.com>
2022-07-26 00:49:45 -0400

    Add files via upload

---
+++ InputFiles/InputFiles/InputFiles/MainPage.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using Xamarin.Forms;
+
+namespace InputFiles {
+	public class MainPage : ContentPage {
+		// Within MS VS, the file data.txt has the property "Embedded Resource"
+		// The file is readonly and is not editable.
+		// Is located in the shared project within MS VS.
+		const string FNAME = "data.txt";
+		public Label edit;
+		private static string GetFileContents(string fname) {
+			var assembly = IntrospectionExtensions.GetTypeInfo(typeof(MainPage)).Assembly;
+			string namespaceName = "InputFiles";
+			Stream stream = assembly.GetManifestResourceStream(namespaceName + "." + fname);
+			string text = "";
+			using (var dictReader = new System.IO.StreamReader(stream)) {
+				text = dictReader.ReadToEnd();
+			}
+			return text;
+		}
+		public MainPage() {
+			StackLayout layout = new StackLayout();
+			edit = new Label { Text = GetFileContents("data.txt") };
+			layout.Children.Add(edit);
+			layout.Padding = new Thickness(0, 40, 0, 0);
+			Content = layout;
+		}
+	}
+}
