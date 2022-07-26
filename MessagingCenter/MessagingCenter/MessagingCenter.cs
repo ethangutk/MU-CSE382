f8cc6007f19bf661f477c9ee18f99b491828950a
Ethan Gutknecht <93880357+ethangutknecht@users.noreply.github.com>
2022-07-26 00:31:24 -0400

    Add files via upload

---
+++ MessagingCenter/MessagingCenter/MessagingCenter.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using Xamarin.Forms;
+
+namespace Messaging {
+	public class Producer {
+		private string name;
+		private int id1;
+		private int id2;
+		public Producer(string name) {
+			this.name = name;
+			id1 = 0;
+			id2 = 0;
+		}
+		public void Trigger1() {
+			Console.WriteLine(name + " sending message type #1 " + ++id1);
+			MessagingCenter.Send<Producer, int>(this, "Event1", id1);
+		}
+		public void Trigger2() {
+			Console.WriteLine(name + " sending message type #2 " + ++id2);
+			MessagingCenter.Send<Producer, int>(this, "Event2", id2);
+		}
+		public override string ToString() {
+			return name;
+		} 
+	}
+	public class Receiver {
+		public string name;
+		public Receiver(string n) {
+			name = n;
+			MessagingCenter.Subscribe<Producer, int>(this, "Event1", (sender, arg) => {
+				Console.WriteLine(name + " received message " + arg + " from " + sender.ToString());
+				//Thread.Sleep(10000);   // this causes the Producer's to stall
+			});
+		}
+	}
+	public class Program {
+		public static void Main(String [] args) {
+			Producer P1 = new Producer("P1");
+			Receiver R1 = new Receiver("R1");
+			Producer p1 = new Producer("p1");
+			Receiver r1 = new Receiver("r1");
+
+			Producer p2 = new Producer("p2");
+			Receiver r2 = new Receiver("r2");
+
+			p1.Trigger1();
+			p2.Trigger1();
+/*
+			P1.Trigger1();
+			// P1 sending message type #1 1
+			// R1 received message 1 from P1
+
+			P1.Trigger1();
+			// P1 sending message type #1 2
+			// R1 received message 2 from P1
+			Receiver B = new Receiver("R2");
+
+			P1.Trigger1();
+			// P1 sending message type #1 3
+			// R1 received message 3 from P1
+			// R2 received message 3 from P1
+
+			P1.Trigger2();
+			// P1 sending message type #2 1
+*/
+		}
+	}
+}
---
+++ MobileFileSystem/MobileFileSystem/MobileFileSystem/MainPage.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Essentials;
+using System.IO;
+
+namespace MobileFileSystem {
+	// Learn more about making custom code visible in the Xamarin.Forms previewer
+	// by visiting https://aka.ms/xamarinforms-previewer
+	[DesignTimeVisible(false)]
+	public partial class MainPage : ContentPage {
+		public MainPage() {
+			InitializeComponent();
+			LoadFiles();
+		}
+		public static string GetFullFileName(string basename) {
+			string libFolder = FileSystem.AppDataDirectory;
+			string fname = System.IO.Path.Combine(libFolder, basename);
+			return fname;
+		}
+		private void NewClicked(object sender, EventArgs e) {
+			WriteFile(name.Text, contents.Text);
+			LoadFiles();
+		}
+		private void LoadFiles() {
+			string libFolder = FileSystem.AppDataDirectory;
+			string[] files = Directory.GetFiles(libFolder);
+			for (int i = 0; i < files.Length; i++) {
+				char sep = Path.DirectorySeparatorChar;
+				int pos = files[i].LastIndexOf(sep);
+				files[i] = files[i].Substring(pos+1);
+			}
+			filesLV.ItemsSource = files;
+			if (files.Length > 0)
+				filesLV.SelectedItem = files[0];
+		}
+		private void DeleteClicked(object sender, EventArgs e) {
+			string fullName = GetFullFileName((string)filesLV.SelectedItem);
+			File.Delete(fullName);
+			LoadFiles();
+		}
+		private void WriteFile(string fname, string contents) {
+			string fullName = GetFullFileName(fname);
+			StreamWriter sr = new StreamWriter(fullName);
+			sr.Write(contents);
+			sr.Close();
+		}
+		private void UpdateClicked(object sender, EventArgs e) {
+			if (filesLV.SelectedItem == null)
+				return;
+			WriteFile((string)filesLV.SelectedItem, contents.Text);
+		}
+		private void FileSelected(object sender, SelectedItemChangedEventArgs e) {
+			if (filesLV.SelectedItem == null)
+				return;
+			string fname = GetFullFileName((string)filesLV.SelectedItem);
+			StreamReader sr = new StreamReader(fname);
+			contents.Text = sr.ReadToEnd();
+			sr.Close();
+		}
+	}
+}
