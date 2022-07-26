ae62efd941e97e7f90bc3a017a329f6a7a9fafee
Ethan Gutknecht <93880357+ethangutknecht@users.noreply.github.com>
2022-07-26 00:52:43 -0400

    Add files via upload

---
+++ NumberProperties/Evens/EvensDriver.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using NumberProperties;
+
+namespace Evens {
+	public class EvensDriver {
+		public static void Main(String[] args) {
+			Console.WriteLine("Enter positive integers");
+			String[] toks = Console.ReadLine().Split(' ');
+			foreach (String tok in toks) {
+				int v = Int32.Parse(tok);
+				Console.WriteLine("{0} is even {1}", v, IntegerProperties.IsEven(v));
+			}
+		}
+	}
+}
---
+++ NumberProperties/NumberProperties/IntegerProperties.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace NumberProperties {
+	public class IntegerProperties {
+		public static bool IsEven(int N) {
+			return N % 2 == 0;
+		}
+		public static bool IsOdd(int N) {
+			return !IsEven(N);
+		}
+		public static bool IsPrime(int N) {
+			if (N <= 1)
+				return false;
+			if (N == 2)
+				return true;
+			if (N % 2 == 0)
+				return false;
+			int lastDiv = (int)Math.Sqrt(N);
+			for (int div = 3; div <= lastDiv; div += 2) {
+				if (N % div == 0) {
+					return false;
+				}
+			}
+			return true;
+		}
+	}
+}
