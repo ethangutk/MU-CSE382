cd060edadca1a6abd5bd1f68492349e1ee148706
Ethan Gutknecht <93880357+ethangutknecht@users.noreply.github.com>
2022-07-26 00:19:42 -0400

    Add files via upload

---
+++ Audio/Audio/Audio/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Plugin.SimpleAudioPlayer;
+using System.IO;
+using System.Reflection;
+
+namespace Audio {
+    public partial class App : Application {
+        public App() {
+            InitializeComponent();
+            MainPage = new MainPage();
+        }
+        private void PlaySound(string file) {
+            var assembly = typeof(App).GetTypeInfo().Assembly;
+            String thisNameSpace = "Audio";
+            Stream audioStream = assembly.GetManifestResourceStream(thisNameSpace + "." + file);
+            ISimpleAudioPlayer player = CrossSimpleAudioPlayer.CreateSimpleAudioPlayer();
+            player.Load(audioStream);
+            player.Play();
+        }
+        protected override void OnStart() {
+            PlaySound("start.mp3");
+        }
+        protected override void OnSleep() {
+            PlaySound("sleep.mp3");
+        }
+        protected override void OnResume() {
+            PlaySound("resume.mp3");
+        }
+    }
+}
---
+++ Audio/Audio/Audio/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Plugin.SimpleAudioPlayer;
+using System.IO;
+using System.Reflection;
+
+namespace Audio {
+	// Learn more about making custom code visible in the Xamarin.Forms previewer
+	// by visiting https://aka.ms/xamarinforms-previewer
+	[DesignTimeVisible(false)]
+	public partial class MainPage : ContentPage {
+		ISimpleAudioPlayer player = CrossSimpleAudioPlayer.CreateSimpleAudioPlayer();
+		public MainPage() {
+			InitializeComponent();
+			Load("start.mp3");
+		}
+		private void Load(string file) {
+			var assembly = typeof(App).GetTypeInfo().Assembly;
+			String ns = "Audio";
+			Stream audioStream = assembly.GetManifestResourceStream(ns + "." + file);
+			player.Load(audioStream);
+		}
+		private void OnClicked(object sender, EventArgs e) {
+			player.Play();
+		}
+	}
+}
