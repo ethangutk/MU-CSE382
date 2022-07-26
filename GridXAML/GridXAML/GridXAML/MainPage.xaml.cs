3e9a2d32fb436959ace7e0b6bfc015c2dc1b7fcf
Ethan Gutknecht <93880357+ethangutknecht@users.noreply.github.com>
2022-07-26 00:48:19 -0400

    Add files via upload

---
+++ GridXAML/GridXAML/GridXAML/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace GridXAML {
+	// Learn more about making custom code visible in the Xamarin.Forms previewer
+	// by visiting https://aka.ms/xamarinforms-previewer
+	[DesignTimeVisible(false)]
+	public partial class MainPage : ContentPage {
+		public MainPage() {
+			InitializeComponent();
+		}
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+			Button senderbutton = sender as Button;
+			if (resultLabel.Text.Equals("0"))
+            {
+				resultLabel.Text = senderbutton.Text;
+            } else
+            {
+				resultLabel.Text += senderbutton.Text;
+            }
+		}
+
+        private void Clear_Clicked(object sender, EventArgs e)
+        {
+			resultLabel.Text = "0";
+        }
+    }
+}
