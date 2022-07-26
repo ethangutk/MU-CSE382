9574ddb4d01c78e4b301ce4d8a05c226aa825cf8
Ethan Gutknecht <93880357+ethangutknecht@users.noreply.github.com>
2022-07-26 00:55:16 -0400

    Add files via upload

---
+++ Scrolling/Scrolling/Scrolling/MainPage.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Scrolling {
+    class MainPage : ContentPage {
+        public const int N = 10;
+
+        public MainPage() {
+            StackLayout buttonPanel1 = new StackLayout();
+            StackLayout buttonPanel2 = new StackLayout();
+
+            for (int i = 0; i < N; i++) {
+                buttonPanel1.Children.Add(new Button { Text = i.ToString() });
+                buttonPanel2.Children.Add(new Button { Text = ((char)('A' + i)).ToString() });
+            }
+
+            ScrollView scrollView1 = new ScrollView { HeightRequest = 200, VerticalScrollBarVisibility = ScrollBarVisibility.Always };
+            ScrollView scrollView2 = new ScrollView { HeightRequest = 200, VerticalScrollBarVisibility = ScrollBarVisibility.Always };
+
+            scrollView1.Content = buttonPanel1;
+            scrollView2.Content = buttonPanel2;
+
+
+            StackLayout topLevel = new StackLayout();
+            topLevel.Children.Add(scrollView1);
+            topLevel.Children.Add(new BoxView { WidthRequest = 300, Color = Color.Red,
+                                                HeightRequest = 10, HorizontalOptions = LayoutOptions.Center });
+            topLevel.Children.Add(scrollView2);
+
+            this.Content = topLevel;
+        }
+    }
+}
